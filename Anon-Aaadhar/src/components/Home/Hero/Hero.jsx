76cbbaf1eccaa39c7ae7e28dc461fcbf11cbdec7
VineetSalve <vineetsalve0@gmail.com>
2024-07-11 22:54:01 +0530

    Created Home and Hero Page

--- Anon-Aaadhar/src/components/Home/Hero/Hero.jsx
+++ Anon-Aaadhar/src/components/Home/Hero/Hero.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./Hero.css";
+import Button from "../../Button/Button";
 
 function Hero() {
   return (
     <>
-    
+      <div className="heroContainer">
+        <div className="left">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur, soluta. Illo ex ipsa quasi incidunt rerum, molestiae
+            sint unde et beatae assumenda. Libero adipisci, esse explicabo odit
+            nisi ab iusto, harum neque officia praesentium ad Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Sint quasi et necessitatibus
+            repellendus incidunt soluta, non dolorem minus aperiam amet!
+          </p>
+          <span>
+            <Button text={"Get Started"} colour={"blue"} />
+          </span>
+        </div>
+        <div className="right">
+          <img
+            src="/src/assets/homepage.png"
+            style={{ width: "500px", height: "500px" }}
+            alt="homepage"
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- Anon-Aaadhar/src/components/Home/NavBar/NavBar.jsx
+++ Anon-Aaadhar/src/components/Home/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NavBar.css";
+import Button from "../../Button/Button";
 
 function NavBar() {
   return (
@@ -7,19 +8,19 @@ function NavBar() {
       <div className="container">
         <div className="navElements">
           <span>
-            <img src="#" alt="LOGO" />
+            <img
+              src="/src/assets/logo.png"
+              style={{ width: "90px" }}
+              alt="LOGO"
+            />
+            {/* {logo} */}
           </span>
         </div>
+        <div className="navElements" style={{width: '440px'}}>
+          Privacy to Security: Empower your aadhar
+        </div>
         <div className="navElements">
-          <ul>
-            <a href="#">Option 1</a>
-          </ul>
-          <ul>
-            <a href="#">Option 2</a>
-          </ul>
-          <ul>
-            <a href="#">Option 3</a>
-          </ul>
+          <Button text={"Connect Wallet"} colour={"green"} />
         </div>
       </div>
     </>
