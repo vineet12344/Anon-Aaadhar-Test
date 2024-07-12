109a130a2f06eab1c4c075f681606cff75e3210c
YuvrajKhade <khadeyuvraj8@gmail.com>
2024-07-12 18:57:50 +0530

    File drag and drop functionality

--- Anon-Aaadhar/src/App.jsx
+++ Anon-Aaadhar/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Home from './components/Home/Home';
 import Mainpg from './components/MainPage/mainpg'; // Correct the import path and component name
+import DragDrop from './components/FileUpload/DragDrop';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' element={<Mainpg />} /> {/* Use capital letter for component */}
       </Routes>
     </Router>
+    // <DragDrop/>
   );
 }
 
